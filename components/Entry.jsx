95794cbb0e0cf072a2f82ab14d4189260d09fee1
ruthdolf <pruekzorn@gmail.com>
2026-05-13 11:54:20 +0300

    mapping components from a separate data file

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,12 +1,17 @@
+//TEMPLATE
+
 export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-                <img className="main-image" src={props.img.src} alt={props.img.alt}></img>
+                <img 
+                    className="main-image" 
+                    src={props.img.src} 
+                    alt={props.img.alt}></img>
             </div>
 
             <div className="info-container">
-                <img className="marker" src='marker.png' alt="marker"></img>
+                <img className="marker" src='../images/marker.png' alt="marker"></img>
                 <span className="country">{props.country}</span>
                 <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
                 <h2 className="entry-title">{props.title}</h2>
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src='../globe.png' alt='globe'></img>
+            <img src='../images/globe.png' alt='globe'></img>
             <h1>my travel journal.</h1>
         </header>
     )
